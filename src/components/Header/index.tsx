44cc785471f630381411a593f82b1e831abee44f
vitor kravszenko <vitorkravs3@gmail.com>
2024-01-28 23:56:24 -0300

    modified: recomençando estrutura para mobile first

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,58 @@
+import { useState } from "react";
+
+//icons
 import { ButtonIcon } from "../Arrow-icon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedinIn,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+//styles
 import "./styles.scss";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header id="header-container">
-      <img src="logo-img.png" alt="Logo do site" />
-      <nav id="menu-options">
-        <a href="/">O que nós oferecemos</a>
-        <a href="/">Como funciona</a>
-        <a href="/">Portfólio</a>
-      </nav>
-      <button id="button-header-contact" className="btn-face">
-        Contate-nos
-        <ButtonIcon />
+      <img id="logo-img" src="logo-img.png" alt="Logo do site" />
+
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
       </button>
+
+      <div id="nav-container" className={isMenuOpen ? "active" : ""}>
+        <div id="nav-links-container">
+          <nav id="menu-options">
+            <a href="/">O que nós oferecemos</a>
+            <a href="/">Como funciona</a>
+            <a href="/">Portfólio</a>
+          </nav>
+          <div id="button-header-contact">
+            <button className={isMenuOpen ? "active" : ""}>
+              Contate-nos
+              <ButtonIcon />
+            </button>
+          </div>
+          <div id="menu-icons">
+            <a href="/">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+            <a href="/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a href="/">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
