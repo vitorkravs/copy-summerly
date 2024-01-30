10403e9d4c6c178855dca3eb5ec75ba0680fabdb
vitor kravszenko <vitorkravs3@gmail.com>
2024-01-30 01:54:00 -0300

    feat: header mobile finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 //icons
 import { ButtonIcon } from "../Arrow-icon";
@@ -18,22 +18,22 @@ import Button from "../Button/Button";
 const Header = () => {
   //gerencia o estado do menu: ativo(true) ou desativado(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  console.log(isMenuOpen);
   //muda o estado do menu
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   //Se o menu estiver ativo, a opção de touchmove(rolagem da página) é desativada.
-  if (isMenuOpen) {
-    document.body.addEventListener(
-      "touchmove",
-      function (e) {
-        e.preventDefault();
-      },
-      { passive: false }
-    );
-  }
+  useEffect(() => {
+    if (isMenuOpen) {
+      // Adiciona ouvinte de evento para prevenir scroll
+      document.body.style.overflow = "hidden";
+    } else {
+      // Remove ouvinte de evento para permitir scroll
+      document.body.style.overflow = "auto";
+    }
+  }, [isMenuOpen]);
 
   return (
     <header id="header-container">
@@ -93,28 +93,66 @@ const Header = () => {
       </div>
 
       <div id="hero-box">
-        <div className="hero-heading-part-box">
-          <h2>Agência de mídia</h2>
-        </div>
-        <div className="hero-heading-part-box">
-          <h2>Liderada por</h2>
-        </div>
-        <div className="hero-heading-part-box">
-          <h2>Influenciadores</h2>
+        <div>
+          <div className="hero-heading-part-box">
+            <h2>Agência de mídia</h2>
+          </div>
+          <div className="hero-heading-part-box">
+            <h2>Liderada por</h2>
+          </div>
+          <div className="hero-heading-part-box">
+            <h2>Influenciadores</h2>
+          </div>
         </div>
 
-        <div className="hero-animation-box">
+        <div id="hero-animation-box">
           <p>
             Alcance seu público-alvo de maneira genuína por meio de crescimento
-            pago e orgânico em
-            <span>TikTok, Instagram, YouTube, Pinterest</span> and more by a
-            team of content creators with a proven track record.
+            pago e orgânico no{" "}
+            <span id="hero-text-bold">
+              TikTok, Instagram, YouTube, Pinterest
+            </span>{" "}
+            e muito mais por uma equipe de criadores de conteúdo com histórico
+            comprovado.
           </p>
         </div>
 
-        <div id="button hero">
-          <Button text="trabalhe Conosco" />
+        <div id="button-hero">
+          <Button text="Trabalhe Conosco" />
         </div>
+      </div>
+      <div id="img-header-container" className={isMenuOpen ? "active" : ""}>
+        <div className="icon-emoji-shopping">
+          <img
+            src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d187a8a7449241d438c9b9_shopping-bags.png"
+            loading="lazy"
+            alt=""
+            className="hero_shopping"
+          ></img>
+        </div>
+        <div className="icon-emoji-party">
+          <img
+            src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d187a871aadb6252940663_party-popper.png"
+            loading="lazy"
+            alt=""
+            className="hero_party"
+          ></img>
+        </div>
+        <div className="icon-emoji-fire">
+          <img
+            src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1807dd0496a3408c33ef6_fire.png"
+            loading="lazy"
+            alt=""
+            className="hero_fire"
+          ></img>
+        </div>
+
+        <img
+          src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/640e177a346b3466d52f1c92_Final_ILL_FinalRGB.svg"
+          loading="lazy"
+          alt="mulher com celular"
+          className="image-hero"
+        ></img>
       </div>
     </header>
   );
--- src/components/nav-icon-close.tsx
+++ src/components/nav-icon-close.tsx
@@ -7,7 +7,9 @@ export function OpenNav() {
       height="35"
       preserveAspectRatio="xMidYMid meet"
       style={{
-        width: "100%; height: 100%; transform: translate3d(0px, 0px, 0px);",
+        width: "100%",
+        height: "100%",
+        transform: "translate3d(0px, 0px, 0px)",
       }}
     >
       <defs>
@@ -15,20 +17,20 @@ export function OpenNav() {
           <rect width="440" height="440" x="0" y="0"></rect>
         </clipPath>
       </defs>
-      <g clip-path="url(#__lottie_element_2)">
+      <g clipPath="url(#__lottie_element_2)">
         <g
           transform="matrix(-10,0,0,-10,220.01300048828125,219.9980010986328)"
           opacity="1"
-          style={{ display: " block;" }}
+          style={{ display: " block" }}
         >
           <g opacity="1" transform="matrix(1,0,0,1,0,0)">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill-opacity="0"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fillOpacity="0"
               stroke="rgb(26,9,36)"
-              stroke-opacity="1"
-              stroke-width="8"
+              strokeOpacity="1"
+              strokeWidth="8"
               d=" M-6.0289998054504395,6.0289998054504395 C-6.025000095367432,6.025000095367432 -6.019999980926514,6.019999980926514 -6.014999866485596,6.014999866485596"
             ></path>
           </g>
@@ -36,16 +38,16 @@ export function OpenNav() {
         <g
           transform="matrix(10,0,0,10,220.01300048828125,219.9980010986328)"
           opacity="1"
-          style={{ display: " block;" }}
+          style={{ display: " block" }}
         >
           <g opacity="1" transform="matrix(1,0,0,1,0,0)">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill-opacity="0"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fillOpacity="0"
               stroke="rgb(26,9,36)"
-              stroke-opacity="1"
-              stroke-width="8"
+              strokeOpacity="1"
+              strokeWidth="8"
               d=" M-6.0289998054504395,6.0289998054504395 C-6.025000095367432,6.025000095367432 -6.019999980926514,6.019999980926514 -6.014999866485596,6.014999866485596"
             ></path>
           </g>
@@ -53,16 +55,16 @@ export function OpenNav() {
         <g
           transform="matrix(0,-10,10,0,220.01300048828125,219.9980010986328)"
           opacity="1"
-          style={{ display: " block;" }}
+          style={{ display: " block" }}
         >
           <g opacity="1" transform="matrix(1,0,0,1,0,0)">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill-opacity="0"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fillOpacity="0"
               stroke="rgb(26,9,36)"
-              stroke-opacity="1"
-              stroke-width="8"
+              strokeOpacity="1"
+              strokeWidth="8"
               d=" M-6.0289998054504395,6.0289998054504395 C-6.025000095367432,6.025000095367432 -6.019999980926514,6.019999980926514 -6.014999866485596,6.014999866485596"
             ></path>
           </g>
@@ -70,16 +72,16 @@ export function OpenNav() {
         <g
           transform="matrix(0,10,-10,0,220.01300048828125,219.9980010986328)"
           opacity="1"
-          style={{ display: " block;" }}
+          style={{ display: " block" }}
         >
           <g opacity="1" transform="matrix(1,0,0,1,0,0)">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill-opacity="0"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fillOpacity="0"
               stroke="rgb(26,9,36)"
-              stroke-opacity="1"
-              stroke-width="8"
+              strokeOpacity="1"
+              strokeWidth="8"
               d=" M-6.0289998054504395,6.0289998054504395 C-6.025000095367432,6.025000095367432 -6.019999980926514,6.019999980926514 -6.014999866485596,6.014999866485596"
             ></path>
           </g>
