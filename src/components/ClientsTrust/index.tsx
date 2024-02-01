ca7b4e26084d876ca28f53a8b44fe11c57612c67
vitor kravszenko <vitorkravs3@gmail.com>
2024-02-01 18:11:54 -0300

    feat: responsividade do componente ClientsTrust completa

--- src/components/ClientsTrust/index.tsx
+++ src/components/ClientsTrust/index.tsx
@@ -6,45 +6,45 @@ const ClientTrust = () => {
       <h3>Confiado por</h3>
 
       <div id="client-list">
-        <div>
-          <div className="walmart-container">
+        <div className="client-row-one">
+          <div className="client-container">
             <img
               src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d3df6fb38a3f51302d5d00_walmart.svg"
               alt="Logo Walmart"
               className="walmart"
             ></img>
           </div>
-          <div className="grow-therapy-container">
+          <div className="client-container">
             <img
               src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d145151f7d3b68b8eca4f0_grow-therapy.svg"
               alt="Logo Grow Therapy"
               className="grow-therapy"
             ></img>
           </div>
-          <div className="avene-container">
-            <img
-              src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1451541b400081fe498e8_avene.svg"
-              alt="Logo Avéne"
-              className="avene"
-            ></img>
-          </div>
-        </div>
-        <div>
-          <div className="propflex-container">
-            <img
-              src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1451462b7fa42bf6ddf01_popflex.svg"
-              alt=""
-              className="propflex"
-            ></img>
-          </div>
-          <div className="norstrom-container">
+          <div className="client-container">
             <img
               src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d14515ec1e97a2f6bcd98f_nordstrom.svg"
               alt="Logo Norstrom"
               className="norstrom"
             ></img>
           </div>
-          <div className="sweet-sweat-container">
+        </div>
+        <div className="client-row-two">
+          <div className="client-container">
+            <img
+              src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1451541b400081fe498e8_avene.svg"
+              alt="Logo Avéne"
+              className="avene"
+            ></img>
+          </div>
+          <div className="client-container">
+            <img
+              src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1451462b7fa42bf6ddf01_popflex.svg"
+              alt=""
+              className="popflex"
+            ></img>
+          </div>
+          <div className="client-container">
             <img
               src="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1451532a91de6811b67cb_sweet-sweat.svg"
               alt="Logo Sweet Sweat"
