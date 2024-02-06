d19277d3a69893c3e1589ed6cd3411e87802f7e0
vitor kravszenko <vitorkravs3@gmail.com>
2024-02-06 01:00:56 -0300

    feat: resposividade completa

--- src/components/Numbers/index.tsx
+++ src/components/Numbers/index.tsx
@@ -7,39 +7,43 @@ const Numbers = () => {
     <section id="numbers-container">
       <div className="number-wrapper">
         <h3>O que fazemos organicamente</h3>
-        <Card
-          title="167M+"
-          text="visualizações e contagem"
-          emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1807dd0496a3408c33ef6_fire.png"
-        />
-        <Card
-          title="6 Figuras"
-          text="das vendas realizadas"
-          emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817103a283fad7f2c9b8_money-bag.png"
-        />
-        <Card
-          title="1,9 milhão +"
-          text="seguidores ganhos"
-          emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817199200fbcabd3b1bc_chart-increasing.png"
-        />
+        <div className="cards-flex">
+          <Card
+            title="167M+"
+            text="visualizações e contagem"
+            emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1807dd0496a3408c33ef6_fire.png"
+          />
+          <Card
+            title="6 Figuras"
+            text="das vendas realizadas"
+            emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817103a283fad7f2c9b8_money-bag.png"
+          />
+          <Card
+            title="1,9 milhão +"
+            text="seguidores ganhos"
+            emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817199200fbcabd3b1bc_chart-increasing.png"
+          />
+        </div>
       </div>
       <div className="number-wrapper copy">
         <h3>O que fazemos por meio de anúncios pagos</h3>
-        <Card
-          title="630M+"
-          text="visualizações e contagem"
-          emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d187a871aadb6252940663_party-popper.png"
-        />
-        <Card
-          title="1.2 milhões"
-          text="à venda"
-          emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817103a283fad7f2c9b8_money-bag.png"
-        />
-        <Card
-          title="1800+"
-          text="chamadas reservadas para clientes"
-          emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817199200fbcabd3b1bc_chart-increasing.png"
-        />
+        <div className="cards-flex">
+          <Card
+            title="630M+"
+            text="visualizações e contagem"
+            emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d187a871aadb6252940663_party-popper.png"
+          />
+          <Card
+            title="1.2 milhões"
+            text="à venda"
+            emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817103a283fad7f2c9b8_money-bag.png"
+          />
+          <Card
+            title="1800+"
+            text="chamadas reservadas para clientes"
+            emoji="https://uploads-ssl.webflow.com/63d0f8b43ca85422d48a69d6/63d1817199200fbcabd3b1bc_chart-increasing.png"
+          />
+        </div>
       </div>
     </section>
   );
